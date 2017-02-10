136d7fd5e1ffc32d16ca2a3092209f6b417f0a69
Darren Majak <drmajak@ualberta.ca>
2017-02-10 15:43:07 -0700

    Adjusted the printing and sampling frequency. The "pox.update()"function has been moved to run as frequently as possible. There are still some issues.
    Signed-off-by: Darren Majak <drmajak@ualberta.ca>

--- Capstone/OverallRough/IMUConfig.h
+++ Capstone/OverallRough/IMUConfig.h
@@ -3,24 +3,21 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+/* This driver reads raw data from the BNO055
+
+   Connections
+   ===========
+   Connect SCL to analog 5
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+
+   History
+   =======
+   2015/MAR/03  - First release (KTOWN)
+*/
+
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
 Adafruit_BNO055 bno = Adafruit_BNO055();
-///////////////Definitions for LCD//////////////////////////////
-///////////////////////////////////////////////////////////////
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library
-#include <SPI.h>
-
-#define TFT_CS     10
-#define TFT_RST    9
-// you can also connect this to the Arduino reset
-// in which case, set this #define pin to 0!
-#define TFT_DC     6
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
-
-// Option 2: use any pins but a little slower!
-#define TFT_SCLK 13   // set these to be whatever pins you like!
-#define TFT_MOSI 11   // set these to be whatever pins you like!
-float p = 3.1415926;
